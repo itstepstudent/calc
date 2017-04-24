abf25e0b9ca42cd79767af7925072939ba8b04f9
Igor <tereshchenko.igor@gmail.com>
2017-04-24 10:44:32 +0300

    merge

--- Calculator2/Program.cs
+++ Calculator2/Program.cs
@@ -10,8 +10,8 @@ namespace Calculator2
     {
         static void Main(string[] args)
         {
-            //line 1
-            //line 2
+            //line 3
+            
         }
     }
 }
